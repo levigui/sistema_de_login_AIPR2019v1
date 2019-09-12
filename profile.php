18ab93528f909f73931af25e72c3b251018b3bac
Guilherme S. Camargo <guilherme_s_camargo@estudante.sc.senai.br>
2019-09-12 15:38:26 -0300

    Criação de Um carrocel de imagens
    slider.php atraves de Bootstrap

--- profile.php
+++ profile.php
@@ -19,24 +19,24 @@ require_once 'session.php';
 
 <body class="bg-dark">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">LeviGui Systems</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <a class="navbar-brand text-light" href="#">LeviGui Systems</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-light" href="profile.php">Perfil <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link 1</a>
+                    <a class="nav-link text-light" href="slider.php">Fotos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link 2</a>
+                    <a class="nav-link text-light" href="">Link 3</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <?= $usuario ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -55,11 +55,11 @@ require_once 'session.php';
 
     <div class=" mt-4 container ">
         <div class="row">
-            <div class="col-9 border border-primary bg-primary mb-5">
+            <div class="col-9 border border-info bg-info mb-5">
                 <h1 class="text-light  mt-4  font-weight-bolder text-center mb-5 ">Perfil do <?= $usuario ?></h1>
             </div>
-            <div class="col-4 border border-primary  bg-light "> <img src="<?= $imagemUsuario ?>" alt="Avatar" width="250" height="200"></div>
-            <div class="col-6 border border-primary  text-monospace bg-primary">
+            <div class="col-4 border border-info   "> <img src="<?= $imagemUsuario ?>" alt="Avatar" width="250" height="200"></div>
+            <div class="col-6 border border-info  text-monospace bg-info">
                 <h2 class="text-light mt-2 ">Nome: <?= $nome ?></h2>
                 <h2 class="text-light">E-mail: <?= $email ?></h2>
                 <h2 class="text-light ">Data de Cadastro: <?= date('d/m/Y H:i:s', strtotime($dataCriacao)) ?></h2>
---
+++ slider.php
@@ -0,0 +1,101 @@
+<?php
+//Protegendo profile.php só para usuários logados
+require_once 'session.php';
+?>
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Albúm de Fotos</title>
+</head>
+
+<body class="bg-dark">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <a class="navbar-brand text-light" href="#">LeviGui Systems</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link  text-light" href="profile.php">Perfil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  text-light" href="slider.php">Fotos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  text-light" href="#">Link 3</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light " href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?= $usuario ?>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item  " href="#">Config.</a>
+                        <a class="dropdown-item  " href="sair.php">Sair</a>
+                        <a class="dropdown-item  " href="#">Link 3</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="bd-example " style="width: 800px; height: 600px; display:block; margin:auto; margin-top:50px;">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner border border-light">
+                <div class="carousel-item active">
+                    <img src="imagens/cancer.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Darius</h5>
+                        <p>"Meus Lobos Arrancarão os Anjos do Céu"</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="imagens/lux.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Sylas</h5>
+                        <p>"Reduza o Reino Deles a Sinzas"</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="imagens/rato.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Kha'Zix</h5>
+                        <p>"Mudar...Faz Bem.."</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
